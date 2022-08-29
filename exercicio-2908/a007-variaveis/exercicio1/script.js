8b9784f6163795138926dd884ed30ccd7c579ec0
atilabarbosa <atilacabo@gmail.com>
2022-08-29 17:13:48 -0300

    Finalizando exercicio

---
+++ exercicio-2908/a007-variaveis/exercicio1/script.js
@@ -0,0 +1,35 @@
+// Declare uma variável para armazenar um **nome**, sem atribuir um valor
+
+let nomeUsuario 
+
+
+// Declare uma variável para armazenar uma **idade**, sem atribuir um valor
+
+let idadeUsuario 
+
+// Imprima na tela o **tipo** dessas variáveis que acabou de criar, usando o comando `typeof`
+
+console.log(typeof nomeUsuario)
+console.log(typeof idadeUsuario)
+
+/* Depois, disso, atualize o código para **perguntar** ao usuário seu nome e sua idade, 
+atribuindo esses dois valores às variáveis que acabou de criar.*/
+
+ nomeUsuario = prompt("Diga o nome")
+console.log(nomeUsuario)
+
+ idadeUsuario = prompt("Diga a idade")
+console.log(idadeUsuario)
+
+// Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código.
+
+console.log(typeof nomeUsuario)
+console.log(typeof idadeUsuario)
+
+//Para finalizar, imprima na tela a mensagem: "Olá `nome`,  você tem `idade` anos". Onde `nome` e `idade` são os valores que o usuário inseriu
+
+nomeUsuario = prompt("Qual seu nome?")
+console.log(nomeUsuario)
+
+ idadeUsuario = prompt("Qual seu nome?")
+console.log(idadeUsuario)
