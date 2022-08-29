cb0eca7f5fd96cf7b7a72e068b633d5cb0fd0694
atilabarbosa <atilacabo@gmail.com>
2022-08-29 12:24:17 -0300

    subindo arquivo aula 2904

---
+++ exercício-2908/index.js
@@ -0,0 +1,58 @@
+// Oi, Bom dia pessoal. Boa aula a todos!
+
+/*Receita do bolo
+2 xicaras de farinha
+1 xicara de açucar 
+
+console.log("Bom dia turma Ammal")
+
+//prompt ("qual é o seu nome")
+
+const nome = "Atila Barbosa"
+console.log(nome)
+
+let idade = 31
+console.log(idade)
+
+console.log("Meu nome é", nome, "Minha idade é", idade)
+
+let temPet = true
+console.log("Tem pet?", temPet)
+
+const nomePet = "Lua e Estrela"
+console.log("O nome do Pet é", nomePet)
+
+//Verificar Tipos
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof temPet)
+console.log(typeof nomePet) 
+
+const nomePrompt = prompt("Qual seu nome?")
+console.log(nomePrompt)
+
+let idade = prompt("Qual sua idade?")
+console.log(idade)
+
+console.log(typeof nomePrompt)
+console.log(typeof idade)
+
+//converter tipo: numer -> string
+
+let numero = 10
+console.log(typeof numero, numero)
+
+numero = String(numero)
+console.log(typeof numero, numero) */
+
+const nomeObjeto = "Garrafa Pacco"
+console.log(nomeObjeto)
+console.log(typeof nomeObjeto)
+
+let idadeObjeto = 4
+console.log(idadeObjeto)
+console.log(typeof idadeObjeto)
+
+let temQualidade = true
+console.log(temQualidade)
+console.log(typeof temQualidade)
